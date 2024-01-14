fc3f43b97aebb232fb4daedb84655527e1ffac64
mohyidinhawary <mohyidin2022@gmail.com>
2024-01-14 13:19:19 +0300

    project architecture

--- Digital-Consluting-Plattform/src/App.tsx
+++ Digital-Consluting-Plattform/src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-600">
       Simple React Typescript Tailwind Sample
     </h1>
+    </div>
+    
   );
 }
 
